yrro/NetworkManager

commit 28274495d61cb434272e0e2307dfd824ee78de8e

device/team: always try to connect to teamd in update_connection()
update_connection() may be called during startup when the bus watch
hasn't notified yet the presence (or absence) of the teamd service on
the bus. Try to obtain a connection to the service in order to
retrieve the current configuration.

src/devices/team/nm-device-team.c
@@ -51,6 +51,7 @@ typedef struct {
 	guint teamd_process_watch;
 	guint teamd_timeout;
 	guint teamd_dbus_watch;
+	gboolean teamd_dbus_name_owned;
 } NMDeviceTeamPrivate;
 
 static gboolean teamd_start (NMDevice *device, NMSettingTeam *s_team);
@@ -160,12 +161,11 @@ update_connection (NMDevice *device, NMConnection *connection)
 	}
 	g_object_set (G_OBJECT (s_team), NM_SETTING_TEAM_CONFIG, NULL, NULL);
 
-	if (priv->tdc) {
+	if (ensure_teamd_connection (device)) {
 		const char *config = NULL;
 		int err;
 
-		err = teamdctl_config_get_raw_direct (NM_DEVICE_TEAM_GET_PRIVATE (device)->tdc,
-		                                      (char **)&config);
+		err = teamdctl_config_get_raw_direct (priv->tdc, (char **)&config);
 		if (err == 0)
 			g_object_set (G_OBJECT (s_team), NM_SETTING_TEAM_CONFIG, config, NULL);
 		else
@@ -296,6 +296,7 @@ teamd_dbus_appeared (GDBusConnection *connection,
 	gboolean success;
 
 	g_return_if_fail (priv->teamd_dbus_watch);
+	priv->teamd_dbus_name_owned = TRUE;
 
 	_LOGI (LOGD_TEAM, "teamd appeared on D-Bus");
 	nm_device_queue_recheck_assume (device);
@@ -349,7 +350,7 @@ teamd_dbus_vanished (GDBusConnection *dbus_connection,
 
 	g_return_if_fail (priv->teamd_dbus_watch);
 
-	if (!priv->tdc) {
+	if (!priv->teamd_dbus_name_owned) {
 		/* g_bus_watch_name will always raise an initial signal, to indicate whether the
 		 * name exists/not exists initially. Do not take this as a failure if it hadn't
 		 * previously appeared.
@@ -357,6 +358,8 @@ teamd_dbus_vanished (GDBusConnection *dbus_connection,
 		_LOGD (LOGD_TEAM, "teamd not on D-Bus (ignored)");
 		return;
 	}
+
+	priv->teamd_dbus_name_owned = FALSE;
 
 	_LOGI (LOGD_TEAM, "teamd vanished from D-Bus");
 	teamd_cleanup (device, TRUE);

commit e70640671507ebeeb3a73620f4819f1359429cce

team: fix handling of D-Bus name watch events
When a device is activated any existing teamd instance is killed. But
since commit 28274495d61c ("device/team: always try to connect to
teamd in update_connection()") the disappearing of the D-Bus name
owner always triggers an automatic restart of the instance in
teamd_dbus_vanished() if the name was previously owned. This new
instance conflicts with the instance we're about to start.

Instead, restore the previous behavior of restarting teamd only if
there is an activation in progress and use @tdc as a flag. This also
means that update_connection() should not modify the value of @tdc.

Fixes: 28274495d61cb434272e0e2307dfd824ee78de8e

src/devices/team/nm-device-team.c
@@ -55,7 +55,6 @@ typedef struct {
 	guint teamd_process_watch;
 	guint teamd_timeout;
 	guint teamd_dbus_watch;
-	gboolean teamd_dbus_name_owned;
 	char *config;
 } NMDeviceTeamPrivate;
 
@@ -180,6 +179,7 @@ update_connection (NMDevice *device, NMConnection *connection)
 	NMDeviceTeam *self = NM_DEVICE_TEAM (device);
 	NMSettingTeam *s_team = nm_connection_get_setting_team (connection);
 	NMDeviceTeamPrivate *priv = NM_DEVICE_TEAM_GET_PRIVATE (self);
+	struct teamdctl *tdc = priv->tdc;
 
 	if (!s_team) {
 		s_team = (NMSettingTeam *) nm_setting_team_new ();
@@ -189,6 +189,13 @@ update_connection (NMDevice *device, NMConnection *connection)
 	/* Read the configuration only if not already set */
 	if (!priv->config && ensure_teamd_connection (device))
 		teamd_read_config (device);
+
+	/* Restore previous tdc state */
+	if (priv->tdc && !tdc) {
+		teamdctl_disconnect (priv->tdc);
+		teamdctl_free (priv->tdc);
+		priv->tdc = NULL;
+	}
 
 	g_object_set (G_OBJECT (s_team), NM_SETTING_TEAM_CONFIG, priv->config, NULL);
 }
@@ -321,7 +328,6 @@ teamd_dbus_appeared (GDBusConnection *connection,
 	gboolean success;
 
 	g_return_if_fail (priv->teamd_dbus_watch);
-	priv->teamd_dbus_name_owned = TRUE;
 
 	_LOGI (LOGD_TEAM, "teamd appeared on D-Bus");
 	nm_device_queue_recheck_assume (device);
@@ -384,7 +390,7 @@ teamd_dbus_vanished (GDBusConnection *dbus_connection,
 
 	g_return_if_fail (priv->teamd_dbus_watch);
 
-	if (!priv->teamd_dbus_name_owned) {
+	if (!priv->tdc) {
 		/* g_bus_watch_name will always raise an initial signal, to indicate whether the
 		 * name exists/not exists initially. Do not take this as a failure if it hadn't
 		 * previously appeared.
@@ -392,8 +398,6 @@ teamd_dbus_vanished (GDBusConnection *dbus_connection,
 		_LOGD (LOGD_TEAM, "teamd not on D-Bus (ignored)");
 		return;
 	}
-
-	priv->teamd_dbus_name_owned = FALSE;
 
 	_LOGI (LOGD_TEAM, "teamd vanished from D-Bus");
 	teamd_cleanup (device, TRUE);